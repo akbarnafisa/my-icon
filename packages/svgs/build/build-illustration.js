2ad1cd25b364dafabbe92b1bcef0098678bb5982
Akbar Nafisa Ja’far <akbar.nafisa@dana.id>
2021-04-26 21:43:48 +0700

    feat: add logic to generate icons and illustrations

--- packages/svgs/build/build-illustration.js
+++ packages/svgs/build/build-illustration.js
@@ -3,13 +3,13 @@ const fse = require('fs-extra')
 const config = require('./config.js').illustration
 const chalk = require('chalk')
 
-const generateVue = ({ svg, filename }) => `
+const generateVue = ({ asset, filename }) => `
 <template>
   <img
     :alt="$options.name"
     :width="width || size || null"
     :height="height || size || null"
-    src="@/${svg}"
+    src="@/${asset}"
   />
 </template>
 
@@ -37,21 +37,42 @@ console.log(chalk.black.bgGreen.bold('Generate Illustration'))
 
 globby([config.input]).then(icon => {
   try {
+    const illustrationsFiles = []
+
     icon.forEach(v => {
       let filename = v.match(/([^\/]+)(?=\.\w+$)/)[0]
-      const svgVueFile = generateVue({ filename, svg: v })
-      const outputPath = v.match('assets/illustrations(.*).svg')[1]
+      const assetVueFile = generateVue({ filename, asset: v })
+      const outputPath = v.match('assets/illustrations(.*).(svg|png)')[1]
 
       fse
-        .outputFile(`${config.output}${outputPath}.vue`, svgVueFile)
+        .outputFile(`${config.output}${outputPath}.vue`, assetVueFile)
         .then(() => {
           console.log(`    ${chalk.green('√')} ${filename}`)
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(`    ${chalk.red('X')} ${filename}`)
           console.log(error)
         })
+      illustrationsFiles.push({
+        name: filename,
+      })
     })
+
+    const illustrationsInfo = {
+      illustrationsCount: illustrationsFiles.length,
+      illustrations: illustrationsFiles.sort((a, b) => {
+        if (a.name === b.name) {
+          return 0
+        }
+        return a.name < b.name ? -1 : 1
+      }),
+    }
+
+    // generate illustrations.json
+    fse.outputFile(
+      `${config.output}/illustrations.json`,
+      JSON.stringify(illustrationsInfo, null, 2)
+    )
   } catch (error) {
     console.log(`    ${chalk.red('X')} Failed`)
     console.log(error)
