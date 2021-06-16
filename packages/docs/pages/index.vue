5208430d93e23a866a1c54df1f4e7ac2fe3cb28a
Akbar Nafisa Ja’far <akbar.nafisa@dana.id>
2021-06-16 15:35:30 +0700

    feat: update getting started docs

--- packages/docs/pages/index.vue
+++ packages/docs/pages/index.vue
@@ -13,7 +13,7 @@
     - `build:svgs` - Compile the `vue` file of icons and illustration into `esm` and `cjs`
     - `dev:docs` - Run documentation of the icon, see and search
 
-    ## Adding icons or illustrations
+    ## Adding Icons or Illustrations
 
     - Checkout from the `main` branch then create a new local branch, make sure your branch using `feat` prefix.
       ```bash
@@ -29,7 +29,7 @@
     ### Icons
     - An assets can be categorized as an icon if the color and the size can be modified
     - An icon must be in `.svg` format, for icon we recommend the size is not exceed 1 Kb
-    - Icons are placed in the `packages/svgs/assets/icons` folder, use prefix `Icon` to name the icon
+    - Icons are placed in the `packages/svgs/assets/icons` folder, use prefix `Icon` to name the icon and use camel case format
     - If the asset has not been optimized, you can compress the assets by running command `optimizie-svgs` in the root project
     - Make sure that your icon has `width`, `height`, `view-box`, and `fill` attributes, these attributes will be replaced in `generate-svgs` command
     - After adding new assets, execute the following commands
@@ -47,7 +47,7 @@
 
      ### Illustrations
     - An illustration can be as `.svg` or `.png` format, for illustration we recommend the size is not exceed 10 Kb and use `.svg` format for smaller file size
-    - Illustrations are placed in the `packages/svgs/assets/illustrations` folder, use prefix `Illus` to name the icon
+    - Illustrations are placed in the `packages/svgs/assets/illustrations` folder, use prefix `Illus` to name the icon and use camel case format
     - If the asset has not been optimized, you can compress the assets by running command `optimizie-svgs` in the root project
     - After adding new assets, execute the following commands
     ```bash
@@ -62,5 +62,7 @@
     ```
     - Commit your changes then create pull request to `main` branch
 
+    ## Moving or Delete Icons or Illustrations
+    - To move or delete icons or illustrations, you need to move or delete the `.vue` file in folder `components` then modify the file in `assets` folder
   </div>
 </template>
