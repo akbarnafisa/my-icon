bdbaa8cd33ef634f112962ac09df356253f25646
Akbar Nafisa Ja’far <akbar.nafisa@dana.id>
2021-11-12 11:32:47 +0700

    build: fix mapping data

--- packages/svgs/exporter/getCommitLabel.js
+++ packages/svgs/exporter/getCommitLabel.js
@@ -5,14 +5,14 @@ const figmaRestApi = require('./util/figmaRestApi')
 
 const fetchVersion = async id => {
   return await figmaRestApi.get(
-    'images/' + process.env.FIGMA_PROJECT_ID + '/versions'
+    'files/' + process.env.FIGMA_PROJECT_ID + '/versions'
   )
 }
 
 const getVersion = async () => {
   try {
     const response = await fetchVersion()
-    const versions = response.versions
+    const versions = response.data.versions
     if (versions && versions.length > 0) {
       return versions.filter(v => v.label)[0].description
     }
